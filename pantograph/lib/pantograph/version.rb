78db20e97e3a491530c52dd9226071895edfc9db
John Knapp <knappj2@gmail.com>
2019-12-03 16:14:07 -0500

    fourth commit

--- pantograph/lib/pantograph/version.rb
+++ pantograph/lib/pantograph/version.rb
@@ -1,5 +1,5 @@
 module Pantograph
-  VERSION             = '0.1.2'.freeze
+  VERSION             = '0.1.4'.freeze
   DESCRIPTION         = 'A way to automate your apps'.freeze
   RUBOCOP_REQUIREMENT = '0.49.1'.freeze
 end
